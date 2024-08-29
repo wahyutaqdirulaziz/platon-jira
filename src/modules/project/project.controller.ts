25e31d5aadb543b2dd931ea80655c94a0caea763
WAHYUDEVLOMBOK\wahyu <wahyu.aziz@inteleq.id>
2024-08-29 23:33:57 +0700

    fix

--- src/modules/project/project.controller.ts
+++ src/modules/project/project.controller.ts
@@ -2,7 +2,7 @@ import { Body, Controller, Get, Post, UseInterceptors } from "@nestjs/common";
 import { Project } from "@prisma/client";
 import { ProjectService } from "./project.service";
 import { ApiTags } from "@nestjs/swagger";
-import { ResponseInterceptor } from "../../shared/interceptors/response.interceptor";
+import { ResponseInterceptor } from "../response.interceptor";
 import { CreateProjectDto } from "./dto/createProject.dto";
 
 
